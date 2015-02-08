790a58167943f52005495decf362196105418790
Rafael Laurindo <rafaelplaurindo@gmail.com>
2015-02-08 04:30:44 -0200

    renomeia app mais uma vez

--- lib/interfaces_do/rails.rb
+++
@@ -1,1 +0,0 @@
-require 'interfaces_html/rails/engine'
--- lib/interfaces_do/rails/engine.rb
+++
@@ -1,7 +0,0 @@
-module InterfacesDo
-  module Rails
-    class Engine < ::Rails::Engine
-      isolate_namespace InterfacesDo::Rails
-    end
-  end
-end
