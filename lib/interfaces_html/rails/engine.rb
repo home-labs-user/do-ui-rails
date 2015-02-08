e34e1ac7afc4677026f46c68440396eb797e4f83
Rafael Laurindo <rafaelplaurindo@gmail.com>
2015-02-08 04:23:39 -0200

    remove arquivos renomeados

--- lib/interfaces_html/rails/engine.rb
+++
@@ -1,7 +0,0 @@
-module InterfacesHtml
-  module Rails
-    class Engine < ::Rails::Engine
-      isolate_namespace InterfacesHtml::Rails
-    end
-  end
-end
--- lib/interfaces_html/rails/version.rb
+++
@@ -1,5 +0,0 @@
-module InterfacesHtml
-  module Rails
-    VERSION = "0.0.1"
-  end
-end
